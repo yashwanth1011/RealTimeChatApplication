457b6a051e384d71c3a8cb839c36a3f169239df1
yashwanth1011 <yashwanthchennu1999@gmail.com>
2024-09-21 17:10:49 -0400

    2. Add Form files

--- client/src/Components/Input/index.js
+++ client/src/Components/Input/index.js
@@ -6,12 +6,15 @@ const Input = ({
     type = 'text',
     className = '',
     isRequired = false,
-    placeholder = ''
+    placeholder = '', 
+    value = '',
+    onChange = () => {},
+
 }) => {
   return (
     <div>
-        <label for={name} className="block text-sm font-medium text-gray-800">{name}</label>
-        <input type={type} id={name} className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${className}`} placeholder={placeholder} required = {isRequired}/>
+        <label htmlFor={name} className="block text-sm font-medium text-gray-800">{name}</label>
+        <input type={type} id={name} className={` w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 ${className}`} placeholder={placeholder} required = {isRequired} value = {value} onChange={onChange}/>
     </div>
   )
 }
--- client/src/modules/Form/index.js
+++ client/src/modules/Form/index.js
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Input from "../../Components/Input"
-export const Form = () => {
-  return (
-    <div className='bg-white w-[600px] h-[800px] shadow-lg rounded-lg flex flex-col justify-center items-center'>
-        <div className='text-4xl font-extrabold'>Welcome</div>
-        <div className='text-xl font-light mb-10'>Sign up for Now</div>
-        <Input label='Full Name' name = 'Full Name' type = 'text' placeholder='Enter your full name' className=' mb-6'/>
-        <Input label='Email Address' name = 'Email' type = 'email' placeholder='Enter Email Address' className=' mb-6'/>
-        <Input label='Password' name = 'Password' type = 'password' placeholder='Enter Password' className=' mb-6'/>
-        
-    </div>
-  )
+import Button from '../../Components/Button'
+export const Form = ({
+    isSignInPage = true,
+}) => {
+
+    const [data, setData] = useState({
+        ...(!isSignInPage && { fullname: '' }),
+
+        email: '',
+        password: ''
+    })
+
+    console.log(data)
+
+
+    return (
+        <div className='bg-white w-[800px] h-[800px] shadow-lg rounded-lg flex flex-col justify-center items-center'>
+            <div className='text-4xl font-extrabold'>Welcome {isSignInPage ? 'back' : ''}</div>
+            <div className='text-xl font-light'>
+                <div className='mb-10'> <center>{isSignInPage ? 'Sign in to explore' : 'Sign up for Now to get started'}</center></div>
+                <form onSubmit={(e) => {
+                        e.preventDefault();
+                        console.log("Form submitted", data);
+                    }}>
+                    {isSignInPage ? '' : <Input label='Full Name' name='Full Name' type='text' placeholder='Enter your full name' className=' mb-6' value={data.fullname} onChange={(e) => setData({ ...data, fullname: e.target.value })} />}
+                    <Input label='Email Address' name='Email' type='email' placeholder='Enter Email Address' className=' mb-6' value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
+                    <Input label='Password' name='Password' type='password' placeholder='Enter Password' className=' mb-6' value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
+                    <Button label={isSignInPage ? "Sign Up" : "Sign In"} type="submit" />
+                    <div className='text-sm'>{isSignInPage ? "Didn't have an account?" : "Already have an account? "}<span className=' text-primary cursor-pointer underline mb-6'>{isSignInPage ? 'Sign In' : 'Sign Up'}</span></div>
+                </form>
+            </div>
+
+
+
+
+        </div>
+    )
 }
